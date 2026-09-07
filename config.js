d0e8682b4d3d107d8567d0c5578a20113c47ee79
Roberto Glez <robertogonzalezdiaz8@gmail.com>
2026-09-07 13:00:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuración de Supabase.
 // La anon/publishable key NO es un secreto. La seguridad se consigue con RLS en Supabase.
 // Sustituye estos valores por los de tu proyecto.
-window.SUPABASE_URL = "https://uckybsloqwnruxhwrtoa.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_fecTys35PA55JRiNgz2zow_NWiPx5mp";
+window.SUPABASE_URL = "https://zscahalavxgdpnbztqah.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_W7dHIBbtgmTqKGVjaKTDAw_ccnoCUDu";
